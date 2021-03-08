f5a630fd0bdc60ba8e4da62daec851c6064e94cf
gabrielpereiraalves <gabrielpereiraalves@hotmail.com>
2021-03-08 13:52:08 -0300

    created person class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,13 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var name: String = "Gabriel"
+    var cpf: String = "133.155.233.12"
+}
+
+fun main() {
+    val Gabriel = Pessoa()
+
+    println(Gabriel.name)
+    println(Gabriel.cpf)
 }
