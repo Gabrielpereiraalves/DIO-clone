ef40dc7ac52ca22e98b2503060ac4f0b0dff53e8
gabrielpereiraalves <gabrielpereiraalves@hotmail.com>
2021-03-08 17:58:22 -0300

    Updated to talk about classmembers

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,12 +4,15 @@ class Pessoa {
     var name: String = "Gabriel"
     var cpf: String = "133.155.233.12"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$name e $cpf"
+
 }
 
 fun main() {
     val Gabriel = Pessoa()
+    println(Gabriel.pessoaInfo())
 
-    println(Gabriel)
-    println(Gabriel.name)
-    println(Gabriel.cpf)
 }
