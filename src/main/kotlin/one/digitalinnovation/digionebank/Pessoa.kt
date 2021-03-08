6cc014ef18d3f9f4ac8ec4ff238bc528e9c54426
gabrielpereiraalves <gabrielpereiraalves@hotmail.com>
2021-03-08 17:36:22 -0300

    updated to apply the private modifier to set function fo cpf propertyof cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,11 +3,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var name: String = "Gabriel"
     var cpf: String = "133.155.233.12"
+    private set
 }
 
 fun main() {
     val Gabriel = Pessoa()
 
+    println(Gabriel)
     println(Gabriel.name)
     println(Gabriel.cpf)
 }
